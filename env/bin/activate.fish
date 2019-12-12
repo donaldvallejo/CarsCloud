eec3a77b77f6f99e68f272add442b1281e0c2eb9
Donald Vallejo <donny.vallejo@gmail.com>
2019-12-11 18:39:33 -0800

    pushing to heroku. still have to make the static pages

--- env/bin/activate.fish
+++ env/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/localscrub/dev/makeSchool/SPD/code/Cars/env"
+set -gx VIRTUAL_ENV "/Users/localscrub/dev/makeSchool/SPD/code/CarsCloud/env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
